2fb02e62554218a01a56cc8867740e7856394a18
Nguyễn Lâm <nctl144@gmail.com>
2017-05-27 00:20:27 -0400

    Clean js file

--- static/js/main.js
+++ static/js/main.js
@@ -1,50 +1,62 @@
 ;(function($) {
 	'use strict'
-	var $grid = $('#blog-grid-container').isotope({
-	    itemSelector: '.blog-items',
-		percentPosition: true,
-		layoutMode: 'masonry',
-		masonry: {
-		    columnWidth: '.grid-sizer'
-		}
-	});
+	function isotopeBlog() {
+		var $grid = $('#blog-grid-container').isotope({
+		    itemSelector: '.blog-items',
+			percentPosition: true,
+			layoutMode: 'masonry',
+			masonry: {
+			    columnWidth: '.grid-sizer'
+			}
+		});
 
-	$grid.imagesLoaded().progress( function() {
-	    $grid.isotope('layout');
-	});
+		$grid.imagesLoaded().progress( function() {
+		    $grid.isotope('layout');
+		});
+	}
 
-	var mmenuSelector = $('#menu-mobile').find('nav');
-	var $icon = $("#menu-button-open");
+	function mmenuActivate() {
+		var mmenuSelector = $('#menu-mobile').find('nav');
+		var $icon = $("#menu-button-open");
 
-	mmenuSelector.mmenu({
-		"extensions": [
-            "pagedim-black",
-            "border-offset"
-	    ],
-		offCanvas: {
-			pageSelector: '#page',
-			position: 'right'
-		}
-	});
+		mmenuSelector.mmenu({
+			"extensions": [
+	            "pagedim-black",
+	            "border-offset"
+		    ],
+			offCanvas: {
+				pageSelector: '#page',
+				position: 'right'
+			}
+		});
 
-	var API = mmenuSelector.data("mmenu");
+		var API = mmenuSelector.data("mmenu");
 
-	$icon.on( "click", function() {
-	    API.open();
-	});
-	API.bind( "open:finish", function() {
-	    setTimeout(function() {
-	        $icon.addClass( "is-active" );
-	    }, 100);
-	});
-	API.bind( "close:finish", function() {
-	    setTimeout(function() {
-	        $icon.removeClass( "is-active" );
-	    }, 100);
-	});
+		$icon.on( "click", function() {
+		    API.open();
+		});
+		API.bind( "open:finish", function() {
+		    setTimeout(function() {
+		        $icon.addClass( "is-active" );
+		    }, 100);
+		});
+		API.bind( "close:finish", function() {
+		    setTimeout(function() {
+		        $icon.removeClass( "is-active" );
+		    }, 100);
+		});
+	}
 
-	$('.blog-items').hover(function() {
-		$(this).toggleClass('blog-items-hovered');
+	function blogGridHover() {
+		$('.blog-items').hover(function() {
+			$(this).toggleClass('blog-items-hovered');
+		});
+	}
+
+	$(document).ready(function() {
+		isotopeBlog();
+		mmenuActivate();
+		blogGridHover();
 	});
 
 })(jQuery);
